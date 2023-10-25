a131a9db75ec142b34e6ac6d8e545affbb08f339
alu0101515458 <alu0101515458@ull.edu.es>
2023-10-26 00:14:27 +0100

    adding details

--- practica1/alu.v
+++ practica1/alu.v
@@ -20,11 +20,11 @@ module alu(output wire [3:0] R, output wire zero, carry, sign, input wire [3:0] 
   mux2_4 muxB(salida_mux, A, B, op2_B);            // MUX OP2_B
   mux2_4 muxC(R, salida_sum4, salida_ul4, l);      // MUX OP1, OP2.
   compl1 complementador(salida_Op2, salida_mux, cpl);  // Complementador de 1 bit.
-  sum4 sumador(salida_sum4, carry, salida_Op1, salida_Op2, Cin0);  // Sumador de 4 bits.
+  sum4 sumador(salida_sum4, carry, salida_Op1, salida_Op2, Cin0);  // Sumador de 4 bits. TIENE CARRY.
   ul4 unidad_logica(salida_ul4, salida_Op1, salida_Op2, ALUOp);  // Unidad logica de 4 bits.
 
   // Diseñar las funciones lógicas de los flags.
-  assign sign = R[3];                     // Bit más significativo de R ---> [3]
+  assign sign = R[3];                     // Como no importa, siempre bit más significativo de R ---> [3]
   assign zero = (R == 4'b0000) ? 1 : 0;   // (R == 0) ---> zero = 1 
    
 endmodule
